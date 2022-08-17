9740c63604c30bb9d4d6d528f11329da5020b3f7
VereskQA <ik.veresk@gmail.com>
2022-08-17 17:44:33 +0300

    minor fixes

--- fmh-android/app/src/androidTest/java/ru/iteco/fmhandroid/ui/elements/ControlPanelScreen.java
+++ fmh-android/app/src/androidTest/java/ru/iteco/fmhandroid/ui/elements/ControlPanelScreen.java
@@ -25,7 +25,7 @@ public class ControlPanelScreen {
     public ViewInteraction editNewsButton = onView(withIndex(withId(R.id.edit_news_item_image_view), 0));
     public ViewInteraction newsStatus = onView(withIndex(withId(R.id.news_item_published_text_view), 0));
     public ViewInteraction newsStatusActive = onView(withIndex(withId(R.id.news_item_published_text_view), 0));
-    public ViewInteraction buttonDeleteNews = onView(withId(R.id.delete_news_item_image_view));
+    public ViewInteraction buttonDeleteNews = onView(withIndex(withId(R.id.delete_news_item_image_view), 0));
     public ViewInteraction firstPublicationDateNotActive = onView(withIndex(withId(R.id.news_item_publication_date_text_view), 0));
     public ViewInteraction firstPublicationDateActive = onView(withIndex(withId(R.id.news_item_publication_date_text_view), 0));
     public ViewInteraction buttonEditNewsNotActive = onView(withIndex(withId(R.id.edit_news_item_image_view), 0));
